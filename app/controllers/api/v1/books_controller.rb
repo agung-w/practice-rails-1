13aaf857cb1ee74965654f744b496273655da782
GodGamer69 <74135614+GodGamer69@users.noreply.github.com>
2022-02-07 16:07:40 +0700

    practice

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,45 @@
+
+
+module Api
+  module V1
+    class BooksController < ApplicationController
+      MAX_PAGINATION_LIMIT=100
+      def index
+        books=Book.limit(limit).offset(params[:offset])#ga akan masalah kalo ga ngasih isi param soalnya kalo nill berarti kya g ada batesan gtu
+        render json: BooksRepresenter.new(books).as_json
+      end
+      def create
+        #bsa pake binding.irb buat msk ke irb pas lg jjalanin program
+        author=Author.create!(author_params)
+        book = Book.new(book_params.merge(author_id: author.id))
+        UpdateSkuJob.perform_later(book_params[:title])
+
+        # raise 'exit'
+        if book.save
+          render json: BookRepresenter.new(book).as_json,status: :created
+        else
+          render json: book.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        Book.find(params[:id]).destroy!#tanda seru ini bisa kya buat execption
+        
+        head :no_content
+      # rescue ActiveRecord::RecordNotDestroy #kalo kya gini ada yang kurang karena ini hanya menghandle 1 error
+      #   render json: {},status: :unprocessable_entity
+      end
+      private 
+
+      def limit
+        [params.fetch(:limit,MAX_PAGINATION_LIMIT).to_i,MAX_PAGINATION_LIMIT].min
+      end
+      def book_params
+        params.require(:book).permit(:title)
+      end
+      def author_params
+        params.require(:author).permit(:first_name,:last_name,:age)
+      end
+    end
+  end
+end
---
+++ spec/requests/books_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+describe 'Books API',type: :request do
+    let(:first_author) { FactoryBot.create(:author,first_name:'George',last_name:'The Jungler',age:80) }
+    let(:second_author) { FactoryBot.create(:author,first_name:'Kenny',last_name:'Sudarman',age:78) }
+    describe 'GET /books' do
+        before do
+            FactoryBot.create(:book, title:'1984',author:first_author)
+            FactoryBot.create(:book, title:'360 No Scope',author:second_author)
+        end
+        it 'returns all books' do
+            
+            get '/api/v1/books'
+            
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body).size).to eq(2)
+            expect(response_body).to eq(
+                [
+                    {
+                        'id'=>1,
+                        'title'=>'1984',
+                        'author_name'=>'George The Jungler',
+                        'author_age'=>80
+                    },
+                    {
+                        'id'=>2,
+                        'title'=>'360 No Scope',
+                        'author_name'=>'Kenny Sudarman',
+                        'author_age'=>78
+                    }
+                ]
+            )
+        end
+        it 'returns a subset of books based on limit' do
+            get '/api/v1/books',params: {limit:1}
+            
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body).size).to eq(1)
+            expect(response_body).to eq(
+                [{
+                    'id'=>1,
+                    'title'=>'1984',
+                    'author_name'=>'George The Jungler',
+                    'author_age'=>80
+                }]
+            )
+        end
+        it 'returns a subset of books based on limit and offset' do
+            get '/api/v1/books',params: {limit:1,offset:1}
+            
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body).size).to eq(1)
+            expect(response_body).to eq(
+                [{
+                    'id'=>2,
+                    'title'=>'360 No Scope',
+                    'author_name'=>'Kenny Sudarman',
+                    'author_age'=>78
+                }]
+            )
+        end
+        
+    end
+    describe 'POST /books' do
+        it 'create a new book' do
+            expect{
+                post '/api/v1/books',params:{
+                    book: {title:'The martian'},
+                    author:{first_name:'Andy',last_name:'Weir',age:'48'}
+                }
+            }.to change {Book.count}.from(0).to(1)
+            expect(response).to have_http_status(:created)
+            expect(Author.count).to eq(1)
+            expect(response_body).to eq(
+                {
+                    'id'=>1,
+                    'title'=>'The martian',
+                    'author_name'=>'Andy Weir',
+                    'author_age'=>48
+                }
+            )
+        end
+        
+    end
+    describe 'DELETE /books/:id' do
+        let!(:book){FactoryBot.create(:book, title:'1984',author:first_author)} #kaya cara lain deklarasi
+        #tanda seru dipake biar pas di jalanin tes dijalanin si let juga langsung di jalanin ga nunggu dipanggil di dalem#{}
+        it 'delete a book' do
+            expect{  
+                delete "/api/v1/books/#{book.id}"
+            }.to change{Book.count}.from(1).to(0)
+            
+            expect(response).to have_http_status(:no_content)
+        end
+    end
+end
